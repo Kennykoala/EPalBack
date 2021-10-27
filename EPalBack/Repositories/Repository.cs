44bfc0c0b9236d31676d79f240ff66b9d92d17ac
唐樂維 <le.kentang@gmail.com>
2021-10-27 17:59:52 +0800

    add repository

---
+++ EPalBack/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPalBack.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPalBack.Repositories
+{
+    public class Repository
+    {
+        private readonly EPalContext _context;
+
+        public Repository()
+        {
+            _context = new EPalContext();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Create<T>(T value)  where T :class
+        {
+            _context.Entry(value).State = EntityState.Added;
+        }
+
+        public void Update<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Modified;
+        }
+
+        public void Delete<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Deleted;
+
+        }
+
+        public IQueryable<T>GetAll<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+    }
+}
