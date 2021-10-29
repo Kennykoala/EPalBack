5c2a6a3a47c298d083bb69a4160237b6b696badd
Johnson <johnson1990hitachi@outlook.com>
2021-10-29 16:21:38 +0800

    new  GetMember

---
+++ EPalBack/Controllers/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPalBack.Services;
+using EPalBack.ViewModels.APIBase;
+using EPalBack.ViewModels;
+
+namespace EPalBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly MemberService _memberService;
+
+        public MemberController()
+        {
+            _memberService = new MemberService();
+        }
+
+        [HttpGet]
+        public ApiResonse GetMembers()
+        {
+            try
+            {
+                var result = _memberService.GetMembers();
+                return new ApiResonse(APIStatus.Success, string.Empty, result);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResonse(APIStatus.Fail, ex.Message, null);
+            }
+        }
+    }
+}
